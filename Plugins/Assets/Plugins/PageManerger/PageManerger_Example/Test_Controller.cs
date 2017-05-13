6f47911963f7ae93bbb046ce69fc574793cb57b2
DESKTOP-6A8O42R\Crosis <www30506@gmail.com>
2017-05-13 18:08:42 +0800

    [新增] 版本1.0   頁面管理者跟頁面Base

---
+++ Plugins/Assets/Plugins/PageManerger/PageManerger_Example/Test_Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Controller : MonoBehaviour {
+	
+	void Start () {
+		PageManerger.CloseAllPage ();
+		PageManerger.ChangePage (PageType.Test_MainPage);
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Plugins/Assets/Plugins/PageManerger/PageManerger_Example/Test_MainPage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_MainPage : Page_Base {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	protected override void OnOpen (){
+
+		print ("Test_MainPage Open");
+	}
+
+	protected override IEnumerator IE_OnOpen ()
+	{
+		yield return null;
+		print ("Test_MainPage IE_Open");
+	}
+
+	protected override void OnClose (){
+
+		print ("Test_MainPage Close");
+	}
+
+	protected override IEnumerator IE_OnClose ()
+	{
+		yield return null;
+		print ("Test_MainPage IE_Close");
+	}
+
+
+	public void OnToSecondPage(){
+		PageManerger.ChangePage (PageType.Test_SecondPage);
+	}
+}
