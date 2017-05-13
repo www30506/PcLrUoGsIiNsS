d6d4b9ea634608365f4629e81454d157a8ca91fb
DESKTOP-6A8O42R\Crosis <www30506@gmail.com>
2017-05-13 16:04:52 +0800

    [新增] ParseData第一版本

---
+++ Plugins/Assets/Plugins/ParseData/Example/ParseData_Example.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParseData_Example : MonoBehaviour {
+
+	void Start () {
+		print(PD.DATA ["Example"] ["1"] ["id"]);
+		print(PD.DATA ["Example"] ["1"] ["name"]);
+		print(PD.DATA ["Example"] ["1"] ["price"]);
+		print(PD.DATA ["Example"] ["1"] ["des"]);
+		print(PD.DATA ["Example"] ["2"] ["name"]);
+
+	}
+
+	void Update () {
+		
+	}
+}
