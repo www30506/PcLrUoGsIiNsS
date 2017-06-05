2f70d285f018f6e360a45b09428e0a554ec298bd
DESKTOP-6A8O42R\Crosis <www30506@gmail.com>
2017-06-05 22:55:34 +0800

    [新增] 圖片捲動(Mekudo) [新增] sprite鏡像(Mirror)

---
+++ Plugins/Assets/Plugins/UGUI_Plugins/Scripts/Mekudo/Mekudo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[AddComponentMenu("UI/Effects/Mekudo", 20)]
+public class Mekudo : MonoBehaviour {
+	[SerializeField]private Vector2 second;
+	private float speed_X;
+	private float speed_Y;
+	private RawImage rawImage;
+
+	void Awake(){
+		rawImage = this.GetComponent<RawImage>();
+		SetSpeed();
+	}
+
+	void  Start(){
+	}
+	
+	void Update () {
+		#if UNITY_EDITOR
+		SetSpeed();
+		#endif
+		rawImage.uvRect = new Rect((speed_X * Time.time) %1.0f, (speed_Y* Time.time)%1.0f, 1,1);
+	}
+
+	private void SetSpeed(){
+		if(second.x ==0.0f){
+			speed_X = 0;
+		}
+		else{
+			speed_X = 1/second.x;
+		}
+
+		if(second.y ==0.0f){
+			speed_Y = 0;
+		}
+		else{
+			speed_Y = 1/second.y;
+		}
+	}
+}
