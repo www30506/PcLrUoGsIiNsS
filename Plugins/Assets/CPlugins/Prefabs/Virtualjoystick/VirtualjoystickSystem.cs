2f1ecdef859e03f646cfc50c9d9cea04da7dbc5b
Crowe <www30506@gmail.com>
2018-04-18 15:12:31 +0800

    【新增】 虛擬搖桿 - 取得目前方向

--- Plugins/Assets/CPlugins/Prefabs/Virtualjoystick/VirtualjoystickSystem.cs
+++ Plugins/Assets/CPlugins/Prefabs/Virtualjoystick/VirtualjoystickSystem.cs
@@ -3,24 +3,29 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class VirtualjoystickSystem : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandler {
+	[SerializeField]private Transform joystickTransform;
 	Vector3 startPos;//开始位置
 	Vector3 dir;//方向
-	public float radius = 80;//需要移动的半径，这个要根据自己需要移动的距离改变哦，我的是80
+	[SerializeField]private float radius = 80;//需要移动的半径，这个要根据自己需要移动的距离改变哦，我的是80
 
 	public void OnDrag(PointerEventData eventData){
 		dir = (Input.mousePosition - startPos).normalized;
-		transform.position = Input.mousePosition;
+		joystickTransform.position = Input.mousePosition;
 
-		float dis = Vector3.Distance(transform.position,startPos);
+		float dis = Vector3.Distance(joystickTransform.position,startPos);
 			if (dis > radius)
-				transform.position = startPos + dir * radius;
+			joystickTransform.position = startPos + dir * radius;
 	}
 
 	public void OnEndDrag(PointerEventData eventData){
-			transform.localPosition = Vector3.zero;
+		joystickTransform.localPosition = Vector3.zero;
 	}
 
 	public void OnBeginDrag(PointerEventData eventData){
-		startPos = transform.position;
+		startPos = joystickTransform.position;
+	}
+
+	public Vector2 GetDirection(){
+		return dir; 
 	}
 }
---
+++ Plugins/Assets/CPlugins/Prefabs/Virtualjoystick/VirtualjoystickSystemExample.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualjoystickSystemExample : MonoBehaviour {
+	public VirtualjoystickSystem virtualjoystickSystem;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(virtualjoystickSystem.GetDirection() != Vector2.zero){
+			print(virtualjoystickSystem.GetDirection());
+		}
+	}
+}
