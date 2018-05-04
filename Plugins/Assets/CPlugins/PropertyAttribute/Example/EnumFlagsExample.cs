e3667095add3b72b62298872a835625e86859f0b
Crowe <www30506@gmail.com>
2018-05-04 13:51:09 +0800

    【修改】 資料夾位置 【新增】 EnumFlags Attribute

---
+++ Plugins/Assets/CPlugins/PropertyAttribute/Example/EnumFlagsExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumFlagsExample : MonoBehaviour {
+	[System.Flags] public enum AttackType {
+		// Decimal     // Binary
+		None   = 0,    // 000000
+		Melee  = 1,    // 000001
+		Fire   = 2,    // 000010
+		Ice    = 4,    // 000100
+		Poison = 8     // 001000
+	}
+	[SerializeField] [EnumFlags] AttackType m_flags;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
