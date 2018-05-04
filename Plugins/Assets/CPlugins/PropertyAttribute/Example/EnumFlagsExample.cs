529b97f5eebc00ac3ae7da6d9122f163e62f2675
Crowe <www30506@gmail.com>
2018-05-04 14:18:17 +0800

    【修改】 EnumFlags範例檔案

--- Plugins/Assets/CPlugins/PropertyAttribute/Example/EnumFlagsExample.cs
+++ Plugins/Assets/CPlugins/PropertyAttribute/Example/EnumFlagsExample.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class EnumFlagsExample : MonoBehaviour {
 	[System.Flags] public enum AttackType {
-		// Decimal     // Binary
-		None   = 0,    // 000000
-		Melee  = 1,    // 000001
-		Fire   = 2,    // 000010
-		Ice    = 4,    // 000100
-		Poison = 8     // 001000
+		Melee  = (1 <<0),    // 000001
+		Fire   = (1 <<1),    // 000010
+		Ice    = (1 <<2),    // 000100
+		Poison = (1 <<3)     // 001000
 	}
 	[SerializeField] [EnumFlags] AttackType m_flags;
 
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		if(Input.GetKeyUp(KeyCode.A)){
+			print(m_flags);
+			if((m_flags & AttackType.Melee) == AttackType.Melee){
+				Debug.Log("Melee");
+			}
+			if((m_flags & AttackType.Fire) == AttackType.Fire){
+				Debug.Log("Fire");
+			}
+			if((m_flags & AttackType.Ice) == AttackType.Ice){
+				Debug.Log("Ice");
+			}
+			if((m_flags & AttackType.Poison) == AttackType.Poison){
+				Debug.Log("Poison");
+			}
+		}
 	}
 }
