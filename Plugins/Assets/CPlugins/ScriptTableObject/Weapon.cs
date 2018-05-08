1c76044e0a8892a0acc5fb60507d6408beee5fe9
Crowe <www30506@gmail.com>
2018-05-08 11:24:47 +0800

    【新增】 ScriptTableObject (目前只有網路上的範例檔案 還未整理)

---
+++ Plugins/Assets/CPlugins/ScriptTableObject/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Weapon
+{
+	//[SerializeField]
+	public string weaponName;
+	//[SerializeField]
+	public int damage;
+
+	public Weapon(string name, int dmg)
+	{
+		weaponName = name;
+		damage = dmg;
+	}
+}
