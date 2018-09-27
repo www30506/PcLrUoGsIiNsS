0acc7ab2838980f5bcd08bd5b4eea5bb031d99fb
Crowe <www30506@gmail.com>
2018-09-27 11:23:26 +0800

    【新增】 Google AdMob使用的Script和教學

---
+++ Plugins/Assets/CPlugins/GoogleAdMob/GoogleAdMob.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class GoogleAdMob : MonoBehaviour{
+	private int nowPlatform;
+	[System.Serializable]private class AdMobData{
+		public enum Platform{Android, IOS};
+		public Platform platform;
+		public string appId;
+		public string rewardVideo_AdUnitId;
+		public string Interstitial_AdUnitId;
+		public string testDevice;
+
+		public AdMobData(Platform p_Platform){
+			platform = p_Platform;
+		}
+	}
+
+	[SerializeField]private AdMobData[] AdMobDatas = new AdMobData[]{new AdMobData(AdMobData.Platform.Android), new AdMobData(AdMobData.Platform.IOS)};
+	[SerializeField]private bool testMode = false;
+	public static GoogleAdMob instance;
+
+	private RewardBasedVideoAd rewardBasedVideo;
+	private InterstitialAd interstitial;
+
+	void Awake(){
+		#if UNITY_EDITOR
+		this.gameObject.SetActive (false);
+		#endif
+
+		instance = this;
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	public void Start(){
+		#if UNITY_ANDROID
+		nowPlatform = 0; //Android
+		#elif UNITY_IOS
+		nowPlatform = 1; //IOS
+		#endif
+
+		MobileAds.Initialize(AdMobDatas[nowPlatform].appId);
+
+		/****		獎勵廣告建置	****/
+		this.rewardBasedVideo = RewardBasedVideoAd.Instance;
+
+		rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+		rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+		rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
+		rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
+		rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+		rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+		rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+		this.RequestRewardBasedVideo();
+		/*****************************************/
+
+		/****		插頁廣告建置	****/
+		this.RequestInterstitial();
+		/*******************************/
+	}
+
+	/*************************		獎勵廣告		*******************************/
+	private void RequestRewardBasedVideo(){
+		print("【獎勵廣告】建置");
+
+		if(testMode){
+			AdRequest request = new AdRequest.Builder().AddTestDevice(AdMobDatas[nowPlatform].testDevice).Build();
+			this.rewardBasedVideo.LoadAd(request, AdMobDatas[nowPlatform].rewardVideo_AdUnitId);
+		}
+		else{
+			AdRequest request = new AdRequest.Builder().Build();
+			this.rewardBasedVideo.LoadAd(request, AdMobDatas[nowPlatform].rewardVideo_AdUnitId);
+		}
+	}
+
+	private void HandleRewardBasedVideoLoaded(object sender, EventArgs args){
+		MonoBehaviour.print("【獎勵廣告】Loaded");
+	}
+
+	private void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args){
+		MonoBehaviour.print("【獎勵廣告】FailedToLoad : " + args.Message);
+	}
+
+	private void HandleRewardBasedVideoOpened(object sender, EventArgs args){
+		MonoBehaviour.print("【獎勵廣告】Opened");
+	}
+
+	private void HandleRewardBasedVideoStarted(object sender, EventArgs args){
+		MonoBehaviour.print("【獎勵廣告】Started");
+	}
+
+	private void HandleRewardBasedVideoClosed(object sender, EventArgs args){
+		MonoBehaviour.print("【獎勵廣告】Closed");
+		this.RequestRewardBasedVideo();
+	}
+
+	private void HandleRewardBasedVideoRewarded(object sender, Reward args){
+		string type = args.Type;
+		double amount = args.Amount;
+		MonoBehaviour.print("【獎勵廣告】Rewarded : " + amount.ToString() + " " + type);
+	}
+
+	private void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args){
+		MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
+	}
+	/****************************************************************************/
+
+
+	/***************************		插頁廣告		*****************************/
+
+
+	private void RequestInterstitial(){
+		print("【插頁廣告】建置");
+
+		interstitial = new InterstitialAd(AdMobDatas[nowPlatform].Interstitial_AdUnitId);
+
+		interstitial.OnAdLoaded += HandleOnAdLoaded;
+		interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+		interstitial.OnAdOpening += HandleOnAdOpened;
+		interstitial.OnAdClosed += HandleOnAdClosed;
+		interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+		if(testMode){
+			AdRequest request = new AdRequest.Builder().AddTestDevice(AdMobDatas[nowPlatform].testDevice).Build();
+			interstitial.LoadAd(request);
+		}
+		else{
+			AdRequest request = new AdRequest.Builder().Build();
+			interstitial.LoadAd(request);
+		}
+	}
+
+	private void HandleOnAdLoaded(object sender, EventArgs args){
+		MonoBehaviour.print("【插頁廣告】Loaded");
+	}
+
+	private void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args){
+		MonoBehaviour.print("【插頁廣告】FailedToLoad : "+ args.Message);
+	}
+
+	private void HandleOnAdOpened(object sender, EventArgs args){
+		MonoBehaviour.print("【插頁廣告】Opened");
+	}
+
+	private void HandleOnAdClosed(object sender, EventArgs args){
+		MonoBehaviour.print("【插頁廣告】Closed");
+		interstitial.Destroy ();
+		RequestInterstitial();
+	}
+
+	private void HandleOnAdLeavingApplication(object sender, EventArgs args){
+		MonoBehaviour.print("【插頁廣告】HandleAdLeavingApplication");
+	}
+	/***************************************************************************/
+
+
+	public static void ShowRewardedVideo(){
+		instance.M_ShowRewardedVideo ();
+	}
+
+	public void M_ShowRewardedVideo(){
+		if (rewardBasedVideo.IsLoaded ()) {
+			print ("【獎勵廣告】 展示");
+			rewardBasedVideo.Show ();
+		}
+		else {
+			print ("【獎勵廣告】 未準備好");
+		}
+	}
+
+
+	public static void ShowInterstitialAds(){
+		instance.M_ShowInterstitialAds ();
+	}
+
+	public void M_ShowInterstitialAds(){
+		if (interstitial.IsLoaded ()) {
+			print ("【插頁廣告】 展示");
+			interstitial.Show ();
+		} 
+		else {
+			print ("【插頁廣告】 未準備好");
+		}
+	}
+}
---
+++ Plugins/Assets/CPlugins/GoogleAdMob/ReadMe.txt
@@ -0,0 +1,38 @@
+﻿【1】
+匯入AdMob插件
+https://github.com/googleads/googleads-mobile-unity/releases/tag/v3.15.1
+->googleMobileAds.unitypackage
+
+
+【2】
+在unity匯入安桌需要的額外插件
+Assets -> Play Services Resolver -> Android Resolver -> Resolve
+
+
+【3】
+將CPlugins -> AdMob腳本匯入專案內
+
+
+【4】
+將GoogleAdMob.cs掛在最初始的場景
+
+
+【5】
+在GoogleAdMob.cs 將各平台的appId、rewardVideo_AdUnitId、Interstitial_AdUnitId
+輸入到對應的AdMobDatas
+PS：請不要更改AdMobDatas的platfrom資料
+
+
+【6】
+獎勵型廣告
+展示呼叫ShowRewardedVideo()
+當廣告播放完畢會呼叫HandleRewardBasedVideoRewarded()可以將獲得東西的資料放在這邊
+PS：這不是主執行緒 有些東西無法使用
+請在ShowRewardedVideo延遲0.5秒呼叫另一個function
+再從HandleRewardBasedVideoRewarded 去給定一個值來判斷是否獎勵成功
+
+
+【7】
+插頁型廣告
+展示呼叫ShowInterstitialAds
+要執行的東西直接ShowInterstitialAds裡面就可以了
