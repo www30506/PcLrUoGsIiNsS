fee39618f8a81d99e273ff01236b71312ecfa6d8
Crowe <www30506@gmail.com>
2018-11-06 16:35:45 +0800

    【新增】 自製多語系插件

---
+++ Plugins/Assets/CPlugins/MultiLanguage/MultiLanguage.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public enum LanguageType{English = 1, TraditionalChinese, SimplifiedChinese};
+public class MultiLanguage : MonoBehaviour {
+	[SerializeField] private string textKey;
+	public static LanguageType nowlanguage = LanguageType.English;
+	public static Dictionary<string, Dictionary<string, object>> DATA;
+
+	void Start () {
+		Text _text = this.GetComponent<Text>();
+		if(_text !=null){
+			_text.text = MultiLanguage.GetText(textKey);
+		}
+	}
+
+	void Update () {
+		
+	}
+
+	public static void SetLanguageTxt(string p_StreamingPath){
+		FileStream file = new FileStream(Application.streamingAssetsPath + "/" + p_StreamingPath + ".txt", FileMode.Open, FileAccess.Read);
+		StreamReader sr = new StreamReader(file);
+		DoParseDefineData(sr.ReadToEnd());
+		file.Close();
+		sr.Close();
+	}
+
+	private static void DoParseDefineData(string p_data){
+		DATA = new Dictionary<string, Dictionary<string, object>>();
+		string[] _allLine = p_data.Split('\n');
+		string _key = null;
+
+		for(int i=0; i< _allLine.Length; i++){
+			string[] _strData = _allLine[i].Split('\t');
+			Dictionary<string, object> _data = new Dictionary<string, object> ();
+			//加入key資料
+			_data.Add("Key",_strData[0]);
+
+			for(int j=1; j<_strData.Length; j++){
+				_key = ((LanguageType)j).ToString();
+				_strData[j] = Regex.Replace(_strData[j], "\\s", "");
+				_data.Add (_key, _strData [j]);
+			}
+
+			DATA.Add(_strData[0], _data);
+		}
+	}
+
+	public static void ChangeLanguage(LanguageType p_LanguageType){
+		nowlanguage = p_LanguageType;
+	}
+
+	public static string GetText(string p_key){
+		string _text = DATA[p_key][nowlanguage.ToString()].ToString();
+		return _text;
+	}
+}
---
+++ Plugins/Assets/CPlugins/MultiLanguage/MultiLanguageExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiLanguageExample : MonoBehaviour {
+
+	void Awake(){
+//		MultiLanguage.ChangeLanguage(LanguageType.English);
+		MultiLanguage.SetLanguageTxt("Crosis/Level1");
+		MultiLanguage.ChangeLanguage(LanguageType.TraditionalChinese);
+//		MultiLanguage.ChangeLanguage(LanguageType.SimplifiedChinese);
+	}
+
+	void Start () {
+		print(MultiLanguage.nowlanguage);
+		MultiLanguage.ChangeLanguage(LanguageType.English);
+		print(MultiLanguage.GetText("Level1_Test1"));
+		MultiLanguage.ChangeLanguage(LanguageType.TraditionalChinese);
+		print(MultiLanguage.GetText("Level1_Test1"));
+		MultiLanguage.ChangeLanguage(LanguageType.SimplifiedChinese);
+		print(MultiLanguage.GetText("Level1_Test1"));
+	}
+
+	void Update () {
+		
+	}
+}
